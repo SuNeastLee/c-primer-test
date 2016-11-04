c6c7b1db569580913eb46da2a50529d0147368f7
SuNeastLee <lxd5240l@gmail.com>
2016-11-04 15:29:36 +0800

    Update strucrt_Person.h

--- strucrt_Person.h
+++ strucrt_Person.h
@@ -5,6 +5,11 @@
 #include<string>
 
 struct Person {
+    
+friend std::istream &read(std::istream &, Person &);
+friend std::ostream &print(std::ostream &, const Person &);
+    
+public:
     Person = default;
     Person(const std::string sname, const std::string saddr): name(sname),address(saddr) { }
     Person(std::istream &is){ read(is,*this); }
@@ -12,6 +17,7 @@ struct Person {
     std::string getName() const { return name; }
     std::string getAddr() const { return address; }
     
+private:
     std::string name;
     std::string address;
 };
