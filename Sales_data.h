5a47f33ed294aa0e56ca35412382c23d6207c4de
SuNeastLee <lxd5240l@gmail.com>
2016-11-07 14:36:58 +0800

    Update Sales_data.h

--- Sales_data.h
+++ Sales_data.h
@@ -7,23 +7,33 @@
 #include <iostream>
 
 struct Sales_data {
-friend Sales_data add(const Sales_data&, const Sales_data&);
-friend std::istream &read(std::istream&, Sales_data&);
-friend std::ostream &print(std::ostream&,const Sales_data&);
-public:
-    //构造函数
-    Sales_data = default;
-    Sales_data(const std::string &s): bookNo(s){ }
-    Sales_data(const std::string &s, unsigned n, double p): bookNo(s), units_sold(n), revenue(p*n){ }
-    Sales_data(std::istream &is){ read(i,*this) }
-    //功能函数    
-    std::string isbn() const { return bookNo; }
-    Sales_data& combine(const Sales_data&);
-private:
-    std::string bookNo;
-    unsigned units_sold = 0;
-    double revenue = 0.0;
+    
+    friend Sales_data add(const Sales_data&, const Sales_data&);
+    friend std::istream &read(std::istream&, Sales_data&);
+    friend std::ostream &print(std::ostream&,const Sales_data&);
+    
+    public:
+        //构造函数
+        Sales_data = default;
+        Sales_data(const std::string &s): bookNo(s){ }
+        Sales_data(const std::string &s, unsigned n, double p): bookNo(s), units_sold(n), revenue(p*n){ }
+        Sales_data(std::istream &is){ read(i,*this) }
+        //功能函数    
+        std::string isbn() const { return bookNo; }
+        Sales_data& combine(const Sales_data&);
+        
+    private:
+        std::string bookNo;
+        unsigned units_sold = 0;
+        double revenue = 0.0;
+        inline double avg_price() const;
 };
+
+inline
+double Sales_data::avg_price() const
+{
+    return units_sold ? revenue/units_sold : 0;
+}
 
 Sales_data& Sales_data::combine(const Sales_data& rhs){
     units_sold += rhs.units_sold;
