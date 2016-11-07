d6391124d24561c69544e607cee59d28ca3dbcdc
SuNeastLee <lxd5240l@gmail.com>
2016-11-07 14:22:56 +0800

    Update Screen.h

--- Screen.h
+++ Screen.h
@@ -1,3 +1,6 @@
+#ifndef Screen_and_Window_mgr
+#define Screen_and_Window_mgr
+
 #include<iostream>
 #include<string>
 
@@ -7,27 +10,26 @@ public:
     
     //structure function
     Screen() = default;
-    Screen(pos ht, pos wd);
-    Screen(pos ht, pos wd, char c);
+    Screen(pos ht, pos wd):height(ht),width(wd),contents(ht*wd, ' '){}
+    Screen(pos ht, pos wd, char c):height(ht),width(wd),contents(ht*wd, ' '){}
     
     //function
     void some_member() const;
     char get(){ return contents[cursor]; }
     inline char get(pos hi, pos wi) const;
     Screen &move(pos r, pos c);
+    
 private:
     pos cursor = 0;
     pos height = 0, width = 0;
     std::string contents;
-    mutable size_t access_ctr = 0;//whether set sa
+    mutable size_t access_ctr = 0;//whether set 0 as the value ?
 };
 
 class Window_mgr {
 private:
     std::vector<Screen> screens { Screen(24,80, ' ') };
 };
-
-
 
 
 inline
@@ -46,3 +48,5 @@ void Screen::some_member() const
 {
     ++access_ctr;
 }
+
+#endif
